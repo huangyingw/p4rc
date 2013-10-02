b1576b7367559f0192497275442285699685533f
huangyingw@linux_programming <huangyingw@gmail.com>
2013-10-02 09:48:27 -0700

    add color to the echo output

--- p4u.sh
+++ p4u.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+NC='\033[0;0m' # No Color
 if  ( git status|grep -q 'nothing to commit' )
 then
-  p4 sync -f ...
+    p4 sync -f ...
 else
-  echo the git repository is unclean, please check it before continuing...
+    echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
 fi 
 if  ( git status|grep -q 'nothing to commit' )
 then
-  echo great, the repository is clean, please go ahead...
+    echo -e "${green}great, the repository is clean, please go ahead...  ${NC}"
 else
-  echo the git repository is unclean, please check it before continuing...
+    echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
 fi
