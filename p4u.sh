bb8f3587f53be8303b8cb804f4b559ff6414b57b
huangyingw@linux_programming <huangyingw@gmail.com>
2014-01-17 13:04:45 -0800

    n

--- p4u.sh
+++ p4u.sh
@@ -2,13 +2,21 @@
 red='\033[0;31m'
 green='\033[0;32m'
 NC='\033[0;0m' # No Color
-if  ( git status|grep -q 'nothing to commit' )
+if [ -n "$1" ];
 then
-    p4 sync -f ...
+  folderForP4="$1"...
+  folderForGit="$1"
+else
+  folderForP4=...
+  folderForGit=.
+fi
+if  ( git status "$folderForGit"|grep -q 'nothing to commit' )
+then
+    p4 sync -f "$folderForP4"
 else
     echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
 fi 
-if  ( git status|grep -q 'nothing to commit' )
+if  ( git status "$folderForGit"|grep -q 'nothing to commit' )
 then
     echo -e "${green}great, the repository is clean, please go ahead...  ${NC}"
 else
